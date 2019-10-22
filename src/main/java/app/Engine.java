dbde57e633d71bb2eaffc60de29f43783a39bcbc
georgiev84 <cska30>
2019-10-22 23:56:08 +0300

    Initial commit

---
+++ src/main/java/app/Engine.java
@@ -0,0 +1,17 @@
+package app;
+
+import javax.persistence.EntityManager;
+
+public class Engine implements Runnable{
+
+    public Engine(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    private final EntityManager entityManager;
+
+    @Override
+    public void run() {
+
+    }
+}
